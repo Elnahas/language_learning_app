93015c165c5a2bb8bb180e2aff17ed19a0bb63e7
Hazem <hazemelnahas2010@gmail.com>
2024-06-10 15:12:36 +0300

    - add font weight helper - add some colors - add AppTextStyle for static style text

---
+++ lib/core/theming/app_text_style.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:language_learning_app/core/theming/app_colors.dart';
+
+import '../helper/font_weight_helper.dart';
+
+abstract class AppTextStyle {
+  static TextStyle get font22DarkMedium => GoogleFonts.fredoka(
+      color: AppColors.dark,
+      fontSize: 22.sp,
+      fontWeight: FontWeightHelper.medium);
+
+  static TextStyle get font15Dark60Regular => GoogleFonts.fredoka(
+      color: AppColors.dark60,
+      fontSize: 15.sp,
+      fontWeight: FontWeightHelper.regular);
+
+  static TextStyle get font20WhiteMedium => GoogleFonts.fredoka(
+      color: AppColors.white,
+      fontSize: 20.sp,
+      fontWeight: FontWeightHelper.medium);
+}
