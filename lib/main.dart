f81a1ffa204462247bbf399798a5fa9f1c77f127
Hazem <hazemelnahas2010@gmail.com>
2024-06-10 11:53:57 +0300

    - add ScreenUtil lib and init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:language_learning_app/my_app.dart';
 
-void main() {
+void main() async {
+  await ScreenUtil.ensureScreenSize();
   runApp(const MyApp());
 }
 
