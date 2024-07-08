6b6dce6e5cc95708cdd04d3595cb42df8d7a72d9
Hazem <hazemelnahas2010@gmail.com>
2024-07-08 23:41:21 +0300

    - refactor login screen and split login form , login image and text , social buttons row

--- lib/features/auth/login/ui/screen/login_screen.dart
+++ lib/features/auth/login/ui/screen/login_screen.dart
@@ -1,23 +1,16 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:language_learning_app/core/helper/spacing.dart';
-import 'package:language_learning_app/core/theming/app_colors.dart';
 import 'package:language_learning_app/core/theming/app_text_style.dart';
 import 'package:language_learning_app/features/auth/login/ui/widgets/divider_dotted_line.dart';
 import '../../../../../core/widgets/app_elevated_button.dart';
-import '../../../../../core/widgets/app_text_form_field.dart';
+import '../widgets/login_form.dart';
+import '../widgets/login_image_and_text.dart';
+import '../widgets/social_buttons_row.dart';
 
-class LoginScreen extends StatefulWidget {
+class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  bool isObscureText = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,79 +23,15 @@ class _LoginScreenState extends State<LoginScreen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Align(
-                  alignment: Alignment.center,
-                  child: Image.asset(
-                    "assets/images/login.png",
-                    width: 105.w,
-                    height: 82.h,
-                  ),
-                ),
-                verticalSpace(10),
-                Align(
-                  alignment: Alignment.center,
-                  child: Text(
-                    "For free, join now and\n start learning",
-                    style: AppTextStyle.font22DarkMedium,
-                    textAlign: TextAlign.center,
-                  ),
-                ),
+                LoginImageAndText(),
                 verticalSpace(20),
-                AppTextFormField(
-                    labelText: "Email Address", hintText: "Enter Your Email"),
-                verticalSpace(20),
-                AppTextFormField(
-                  labelText: "Password",
-                  hintText: "Enter Your Password",
-                  obscureText: isObscureText,
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        isObscureText = !isObscureText;
-                      });
-                    },
-                    icon: isObscureText
-                        ? FaIcon(FontAwesomeIcons.eye)
-                        : FaIcon(FontAwesomeIcons.eyeSlash),
-                    iconSize: 16,
-                  ),
-                ),
-                verticalSpace(15),
-                Align(
-                  alignment: AlignmentDirectional.centerEnd,
-                  child: GestureDetector(
-                      onTap: () {},
-                      child: Text(
-                        "Forgot Password",
-                        style: AppTextStyle.font15RedRegular,
-                      )),
-                ),
+                LoginForm(),
                 verticalSpace(30),
                 AppElevatedButton(buttonText: "Login", onPressed: () {}),
                 verticalSpace(20),
                 DividerDottedLine(),
                 verticalSpace(20),
-                Row(
-                  children: [
-                    Expanded(
-                      child: AppElevatedButton(
-                        IconPathSvg: "assets/svgs/ic_facebook.svg",
-                        onPressed: () {},
-                        backgroundColor: AppColors.mercury,
-                        height: 50,
-                      ),
-                    ),
-                    horizontalSpace(10),
-                    Expanded(
-                      child: AppElevatedButton(
-                        IconPathSvg: "assets/svgs/ic_google.svg",
-                        onPressed: () {},
-                        backgroundColor: AppColors.mercury,
-                        height: 50,
-                      ),
-                    ),
-                  ],
-                ),
+                SocialButtonsRow(),
                 verticalSpace(20),
                 Center(
                   child: RichText(
