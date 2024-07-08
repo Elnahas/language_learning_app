d878a6e32a24a76a71a271d3dfc7f5708be25611
Hazem <hazemelnahas2010@gmail.com>
2024-07-08 12:30:06 +0300

    - change style for app bar

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -2,18 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:language_learning_app/core/routing/app_routes.dart';
 import 'package:language_learning_app/core/theming/app_colors.dart';
+import 'package:language_learning_app/core/theming/app_text_style.dart';
 
 import 'core/routing/routes.dart';
 
 class MyApp extends StatelessWidget {
-  final AppRoutes appRoutes ;
+  final AppRoutes appRoutes;
   const MyApp({super.key, required this.appRoutes});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-          designSize: const Size(375, 812),
+      designSize: const Size(375, 812),
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) {
@@ -21,16 +22,20 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           title: 'Language Learning App',
           theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primaryColor),
+            appBarTheme: AppBarTheme(
+                color: AppColors.primaryColor,
+                foregroundColor: Colors.white,
+                centerTitle: true , titleTextStyle: AppTextStyle.font20WhiteMedium),
+            colorScheme:
+                ColorScheme.fromSeed(seedColor: AppColors.primaryColor),
             useMaterial3: true,
           ),
           initialRoute: Routes.onBoarding,
-          onGenerateRoute: appRoutes.onGenerateRoute ,
+          onGenerateRoute: appRoutes.onGenerateRoute,
 
           // home: const OnBoardingScreen(),
         );
       },
-
     );
   }
 }
